f860c7ecf255786a5a4843c5dce32f9954da5d86
Eugene K <evgn.worker@gmail.com>
2023-03-09 23:25:14 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'dart:convert';
-import 'dart:developer';
 import 'package:flutter/material.dart';
+import 'package:flutter_card_swiper/flutter_card_swiper.dart';
 import 'package:http/http.dart' as http;
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,6 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.pink,
       ),
+      debugShowCheckedModeBanner: false,
       home: const MyHomePage(title: 'Chuck Norris tinder'),
     );
   }
@@ -44,14 +46,39 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   int _rate = 0;
-  bool _enableButtons = false;
-  var _joke = '';
+  final CardSwiperController controller = CardSwiperController();
+  String _joke =
+      "Welcome to the chuck Norris tinder!\r\nSwipe right if you like the PUNCHline\r\n👊👊👊👊👊👊👊👊";
   Uri url = Uri.parse('https://api.chucknorris.io/jokes/random');
+  List<Container> cards = [];
 
   @override
   initState() {
+    _addCard(_joke);
+    _addJoke();
     super.initState();
-    _nextJoke();
+  }
+
+  void _addCard(String joke) {
+    cards.add(Container(
+        alignment: Alignment.center,
+        color: Colors.pink,
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Expanded(flex: 2, child: NorrisFace(_counter, _rate)),
+              Expanded(
+                  flex: 1,
+                  child: SingleChildScrollView(
+                      scrollDirection: Axis.vertical,
+                      child: Text(
+                        joke,
+                        style: const TextStyle(
+                          fontSize: 16.0,
+                          color: Colors.white,
+                        ),
+                      )))
+            ])));
   }
 
   void _incrementCounter() {
@@ -59,23 +86,29 @@ class _MyHomePageState extends State<MyHomePage> {
       _counter++;
       _rate++;
     });
-
-    _nextJoke();
   }
 
-  Future<void> _nextJoke() async {
+  Future<String> _addJoke() async {
     var response = await http.get(url);
+    var joke = json.decode(response.body)['value'];
     setState(() {
-      _joke = json.decode(response.body)['value'];
-      _enableButtons = true;
+      _joke = joke;
     });
+    return joke;
   }
 
   void _decrementCounter() {
     setState(() {
       _counter++;
     });
-    _nextJoke();
+  }
+
+  void _createNextCard() {
+    _addCard(_joke);
+    if (cards.isNotEmpty) {
+      cards.removeAt(0);
+    }
+    _addJoke();
   }
 
   @override
@@ -92,51 +125,89 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Expanded(
-                flex:1,
-                child: Text(
-                  '$_rate/$_counter of Chuck Jokes were fun',
-                  style: Theme.of(context).textTheme.bodyMedium,
-                )
-            ),
-            Expanded(
-                flex:6,
-                child: NorrisFace(_counter, _rate)
-            ),
-            Expanded(
-                flex:4,
-                child:  Padding(
-                  padding: const EdgeInsets.all(40),
-                  child: Text(
-                    style: Theme.of(context).textTheme.bodyMedium,
-                    _joke,
+                flex: 1,
+                child: Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Expanded(
+                          flex: 5,
+                          child: Text(
+                            '$_rate/$_counter of Chuck jokes were fun',
+                            style: const TextStyle(
+                              fontSize: 20.0,
+                              color: Colors.black45,
+                            ),
+                          )),
+                      Expanded(
+                          flex: 1,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              showDialog(
+                                  context: context,
+                                  builder: (_) => const AlertDialog(
+                                        title: Text('Developer info'),
+                                        content: DeveloperInfo(),
+                                      ));
+                            },
+                            child: const Icon(Icons.account_circle),
+                          ))
+                    ],
                   ),
-                )
+                )),
+            Expanded(
+              flex: 10,
+              child: CardSwiper(
+                controller: controller,
+                cards: cards,
+                isVerticalSwipingEnabled: false,
+                onSwipe: _swipe,
+                padding: const EdgeInsets.all(24.0),
+              ),
             ),
             Expanded(
-                flex:1,
+                flex: 1,
                 child: Row(
                   children: <Widget>[
                     Expanded(
+                        child: Padding(
+                      padding: const EdgeInsets.all(5),
                       child: ElevatedButton(
-                        onPressed: _enableButtons ? _decrementCounter : null,
+                        onPressed: () {
+                          controller.swipeLeft();
+                        },
                         child: const Icon(Icons.thumb_down),
                       ),
                       // This trailing comma makes auto-formatting nicer for build methods.
-                    ),
+                    )),
                     Expanded(
+                        child: Padding(
+                      padding: const EdgeInsets.all(5),
                       child: ElevatedButton(
-                        onPressed: _enableButtons ? _incrementCounter : null,
+                        onPressed: () {
+                          controller.swipeRight();
+                        },
                         child: const Icon(Icons.thumb_up),
                       ),
-                    )
-                    // This trailing comma makes auto-formatting nicer for build methods.
+                    )),
                   ],
-                )
-            ),
+                )),
           ],
         ),
       )),
     );
+  }
+
+  void _swipe(int index, CardSwiperDirection direction) {
+    _createNextCard();
+    switch (direction) {
+      case CardSwiperDirection.right:
+        _incrementCounter();
+        break;
+      default:
+        _decrementCounter();
+    }
   }
 }
 
@@ -144,7 +215,7 @@ class NorrisFace extends StatefulWidget {
   final int counter;
   final int rate;
 
-  const NorrisFace(this.counter, this.rate);
+  const NorrisFace(this.counter, this.rate, {super.key});
 
   @override
   State<StatefulWidget> createState() => _NorrisFaceState();
@@ -169,10 +240,38 @@ class _NorrisFaceState extends State<NorrisFace> {
   Widget build(BuildContext context) {
     return Image.asset('assets/${getValue()}.webp');
   }
+}
+
+class DeveloperInfo extends StatelessWidget {
+  const DeveloperInfo({super.key});
 
   @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      height: 200,
+      color: Colors.white,
+      child: Column(
+        children: [
+          const Expanded(
+            flex: 3,
+            child: Text('Hello! my name is Eugene, thanks for using my app',
+                style: TextStyle(fontSize: 25)),
+          ),
+          const Expanded(
+              child: Text('Telegram @eukuz', style: TextStyle(fontSize: 20))),
+          Expanded(
+              child: InkWell(
+            onTap: () => launchUrl(
+                Uri.parse('mailto:e.kuzyakhmetov@innopolis.university')),
+            child: const Text(
+              'Or email me here !',
+              style: TextStyle(
+                  decoration: TextDecoration.underline, color: Colors.blue),
+            ),
+          )),
+        ],
+      ),
+    );
   }
 }
